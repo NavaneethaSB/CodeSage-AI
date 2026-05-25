915f4af4a49c8eaaf3223475d59d18475ab1668f
NavaneethaSB <snavaneeth138@gmail.com>
2026-05-25 20:25:05 +0530

    Fixed updated code rendering issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -337,7 +337,7 @@ export default function App() {
 
         <AnimatePresence>
 
-          {updatedCode && (
+          {updatedCode?.trim() !== "" && (
 
             <motion.div
               initial={{ opacity: 0 }}
